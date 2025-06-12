325d6eb2bfa0c570eacbee17fd9fb0928a4773c2
Ajmal <113825950+ajmalajusha@users.noreply.github.com>
2025-06-12 22:22:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/Portfolio" element={<Index />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
